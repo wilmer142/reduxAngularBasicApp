56d06cfa8fb254de0bc70ae8aa18942c29578e3f
wilmer142 <wilmercollege@gmail.com>
2019-12-21 20:25:14 -0500

    creating new components child and grandchild

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ChildComponent } from './counter/child/child.component';
+import { GrandchildComponent } from './counter/grandchild/grandchild.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ChildComponent,
+    GrandchildComponent
   ],
   imports: [
     BrowserModule,
